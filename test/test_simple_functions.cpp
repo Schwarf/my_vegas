d7a3234658461877ddd8407198efe29ac847e416
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-10-14 17:50:05 +0200

    Add another simple integration test, with 2 sigma error.

--- test/test_simple_functions.cpp
+++ test/test_simple_functions.cpp
@@ -16,6 +16,15 @@ double sin_cos_tan(std::vector<double> x, void *param) {
     return jacobi * std::sin(1.0 - x0 * x0) * x1 * std::tan(x2 * x2) * std::tan(x2 * x2);
 }
 
+double sinus(std::vector<double> x, void *param) {
+    constexpr auto pi = std::numbers::pi;
+    auto x0 = x[0];
+    auto x1 = x[1];
+    auto x2 = x[2];
+
+    return std::sin(x0 * x1 * x2);
+}
+
 double polynom(std::vector<double> x, void *param) {
     constexpr auto pi = std::numbers::pi;
     auto x0 = x[0];
@@ -26,7 +35,7 @@ double polynom(std::vector<double> x, void *param) {
 }
 
 
-TEST(PolynomialTest, simple1) {
+TEST(SimpleFunctionTest, polynom) {
     constexpr double expected_result{2.874618986159398};
     constexpr int dimensions{3};
     VEGAS_Integrator integrator;
@@ -36,6 +45,18 @@ TEST(PolynomialTest, simple1) {
     std::cout << integrator.Get_Result() << " +/- " << integrator.Get_Error() << " with chi-square: "
               << integrator.Get_Chisq() << std::endl;
 
-    EXPECT_NEAR(expected_result, integrator.Get_Result(), integrator.Get_Error());
+    EXPECT_NEAR(expected_result, integrator.Get_Result(), 2.0 * integrator.Get_Error());
+}
 
+TEST(SimpleFunctionTest, sinus) {
+    constexpr double expected_result{0.12243402879673784};
+    constexpr int dimensions{3};
+    VEGAS_Integrator integrator;
+    integrator.Set_Integrand(std::move(sinus), dimensions, nullptr);
+    integrator.Improve_Grid();
+    integrator.Integration();
+    std::cout << integrator.Get_Result() << " +/- " << integrator.Get_Error() << " with chi-square: "
+              << integrator.Get_Chisq() << std::endl;
+
+    EXPECT_NEAR(expected_result, integrator.Get_Result(), 2.0 * integrator.Get_Error());
 }
