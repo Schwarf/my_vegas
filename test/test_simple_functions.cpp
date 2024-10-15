240483448c8a588e92404b506fcd43201e7ff692
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-10-15 20:28:38 +0200

    Add one more test function.

--- test/test_simple_functions.cpp
+++ test/test_simple_functions.cpp
@@ -9,13 +9,14 @@
 
 constexpr double sigma_range{3.0};
 
+// This seems a hard function
 double sin_cos_tan(std::vector<double> x, void *param) {
     constexpr auto pi = std::numbers::pi;
     auto x0 = x[0] * pi;
     auto x1 = x[1];
     auto x2 = 2.0 * x[2] - 1.0;
     constexpr auto jacobi = 2.0 * pi;
-    return jacobi * std::sin(1.0 - x0 * x0) * x1 * std::tan(x2 * x2) * std::tan(x2 * x2);
+    return jacobi * std::sin(1.0 - x0 * x0) * std::cos(x1) * std::tan(x2*x2);
 }
 
 double sinus(std::vector<double> x, void *param) {
@@ -43,6 +44,15 @@ double polynom2(std::vector<double> x, void *param) {
     auto x4 = x[4];
     return x3 * x4 * (1.0 + x0 + 2.0 * x1 + x2 * x2) * (1.0 + x0 + 2.0 * x1 + x2 * x2) / (1.0 + x2) / (1.0 + x2) /
            (1.0 + x2);
+}
+
+double log_exp(std::vector<double> x, void *param) {
+    constexpr auto pi = std::numbers::pi;
+    auto x0 = x[0];
+    auto x1 = 2.0*pi*x[1] - pi;
+    auto x2 = 2.0*x[2] - 1.0;
+    constexpr auto jacobi = 2.0*pi*2.0;
+    return jacobi*std::log(x0)*std::exp(x0)*x1*x1/(2.0 + x2*x2*x2);
 }
 
 TEST(SimpleFunctionTest, polynom2) {
@@ -84,3 +94,17 @@ TEST(SimpleFunctionTest, sinus) {
 
     EXPECT_NEAR(expected_result, integrator.get_result(), sigma_range * integrator.get_error());
 }
+
+TEST(SimpleFunctionTest, log_exp) {
+    constexpr double expected_result{-28.37381254755662};
+    constexpr int dimensions{3};
+    VegasNumericalIntegration<dimensions> integrator;
+    integrator.set_integrand(std::move(log_exp), nullptr);
+    integrator.improve_grid();
+    integrator.integrate();
+    std::cout << integrator.get_result() << " +/- " << integrator.get_error() << " with chi-square: "
+              << integrator.get_chisquare() << std::endl;
+
+    EXPECT_NEAR(expected_result, integrator.get_result(), sigma_range * integrator.get_error());
+}
+
