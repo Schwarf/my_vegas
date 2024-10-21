23cc3043a037578c1e55a0138d97531b57ad5977
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-10-21 08:29:27 +0200

    Revert "Breaking changes, everything known at compile time."
    This reverts commit 429fd8452ed32b1cad94a793d80569863710d394.

--- include/VEGAS_Stratify.h
+++ include/VEGAS_Stratify.h
@@ -5,36 +5,42 @@
 #include <cmath>
 #include "misc.h"
 
-
 template<int NumberOfDimensions>
 class VEGAS_Stratify {
-private:
-    static constexpr int InitialNumberOfStratifications{10};
-    static constexpr int MaximumNumberOfHypercubes{10000};
-    static constexpr long long unsigned InitialNumberOfHyperCubes = pow_constexpr(InitialNumberOfStratifications, NumberOfDimensions);
-    static constexpr long long NumberOfStratifications = (InitialNumberOfHyperCubes > MaximumNumberOfHypercubes || NumberOfDimensions > 9) ? ValueMapper<MaximumNumberOfHypercubes, NumberOfDimensions>::NumberOfStratifications : InitialNumberOfStratifications;
-    static constexpr long long NumberOfHyperCubes = (InitialNumberOfHyperCubes > MaximumNumberOfHypercubes || NumberOfDimensions > 9) ? ValueMapper<MaximumNumberOfHypercubes, NumberOfDimensions>::NumberOfHyperCubes : InitialNumberOfHyperCubes;
-
-    double beta{};
-    double V_cubic{};
-    std::array<double, NumberOfHyperCubes> squared_accumulated_function_values;
-    std::array<double, NumberOfHyperCubes> accumulated_function_values;
-    std::array<double, NumberOfHyperCubes> counts;
-    std::array<double, NumberOfHyperCubes> hypercubic_weights;
-    int number_of_expected_evaluations{};
-
 public:
-    VEGAS_Stratify() : beta{0.75}{
-        V_cubic = std::pow(1.0 / NumberOfStratifications, NumberOfDimensions);
-        squared_accumulated_function_values = {};
-        accumulated_function_values = {};
-        counts = {};
-        hypercubic_weights.fill(1.0 / NumberOfHyperCubes);
+    VEGAS_Stratify() : N_STRAT{10}, beta{0.75}, maximum_number_of_hyper_cubes{10000} {
+        // N_STRAT = floor(pow(N_EVALUATES_TRAINED/4.0,1.0/NumberOfDimensions));
+
+        number_of_hyper_cubes = pow(N_STRAT, NumberOfDimensions);
+        // if NumberOfDimensions too large, number_of_hyper_cubes will exceed the MAXIMUM number an integer can store
+        if (number_of_hyper_cubes > maximum_number_of_hyper_cubes || NumberOfDimensions > 9) {
+            N_STRAT = floor(pow(maximum_number_of_hyper_cubes, 1.0 / NumberOfDimensions));
+            number_of_hyper_cubes = pow(N_STRAT, NumberOfDimensions);
+        }
+
+
+        V_cubic = pow(1.0 / N_STRAT, NumberOfDimensions);
+
+        squared_accumulated_function_values = std::vector<double>(number_of_hyper_cubes, 0);
+        accumulated_function_values = std::vector<double>(number_of_hyper_cubes, 0);
+        counts = std::vector<double>(number_of_hyper_cubes, 0);
+        hypercubic_weights = std::vector<double>(number_of_hyper_cubes, 1.0 / number_of_hyper_cubes);
     };
 
     ~VEGAS_Stratify() = default;
 
-
+private:
+    int N_STRAT;
+    double beta{};
+    double V_cubic{};
+    std::vector<double> squared_accumulated_function_values; // size = (N_STRAT)^(NumberOfDimensions)
+    std::vector<double> accumulated_function_values; // size = (N_STRAT)^(NumberOfDimensions)
+    std::vector<double> counts; // size = (N_STRAT)^(NumberOfDimensions)
+    std::vector<double> hypercubic_weights; // size = (N_STRAT)^(NumberOfDimensions)
+    // int N_EVALUATES_TRAINED; // The evaluates number used to train the stratification
+    int number_of_expected_evaluations{};
+    int number_of_hyper_cubes{};
+    int maximum_number_of_hyper_cubes{};
 
 
 public:
@@ -45,8 +51,8 @@ public:
     std::array<int, NumberOfDimensions> get_indices(int index) {
         std::array<int, NumberOfDimensions> indices{};
         for (int i = 0; i < NumberOfDimensions; i++) {
-            int quotient = index / NumberOfStratifications;
-            int remainder = index - quotient * NumberOfStratifications;
+            int quotient = index / N_STRAT;
+            int remainder = index - quotient * N_STRAT;
             indices[i] = remainder;
             index = quotient;
         }
@@ -54,7 +60,7 @@ public:
     }
 
     std::array<double, NumberOfDimensions> get_y(int index, const std::array<double, NumberOfDimensions> &random_uni) {
-        const double dy = 1.0 / NumberOfStratifications;
+        const double dy = 1.0 / N_STRAT;
         std::array<double, NumberOfDimensions> res{};
         std::array<int, NumberOfDimensions> ID = get_indices(index);
         for (int i = 0; i < NumberOfDimensions; i++) {
@@ -74,13 +80,13 @@ public:
     void update_hypercubic_weights() {
         double d_sum = 0;
         double d_tmp;
-        for (int i = 0; i < NumberOfHyperCubes; i++) {
+        for (int i = 0; i < number_of_hyper_cubes; i++) {
             d_tmp = V_cubic * V_cubic / counts[i] * squared_accumulated_function_values[i] -
                     std::pow(V_cubic / counts[i] * accumulated_function_values[i], 2);
             hypercubic_weights[i] = std::pow(d_tmp, beta);
             d_sum += hypercubic_weights[i];
         }
-        for (int i = 0; i < NumberOfHyperCubes; i++) {
+        for (int i = 0; i < number_of_hyper_cubes; i++) {
             hypercubic_weights[i] = hypercubic_weights[i] / d_sum;
         }
     }
@@ -94,7 +100,7 @@ public:
 
     double get_V_cubic() { return V_cubic; }
 
-    int Get_NHYPERCUBICS() { return NumberOfHyperCubes; };
+    int Get_NHYPERCUBICS() { return number_of_hyper_cubes; };
     // void Set_Stratification_System(int NumberOfDimensions, int NEVAL_TRAIN);
 };
 
--- test/test_misc.cpp
+++ test/test_misc.cpp
@@ -6,36 +6,15 @@
 #include <cmath>
 
 
-
 template<int X, int N>
 struct TestNthRoot {
-    static void run() {
-        constexpr int computed_root = nth_root(X, N);
-        int expected_root = static_cast<int>(std::pow(X, 1.0 / N));
-        EXPECT_EQ(computed_root, expected_root);
-    }
+    static constexpr int computed_root = nth_root(X, N);
 };
 
 TEST(ComputeNthRoot, simple1)
 {
-    long long unsigned x{1000};
-
-    for (int i{0}; i < 15; ++i) {
-        x *= 10;
-        for (int n{2}; n < 16; ++n) {
-            // Compute NumberOfStratifications
-            auto NumberOfStratifications = std::floor(std::pow(x, 1.0/n));
-            // Compute NumberOfHyperCubes
-            auto NumberOfHyperCubes = std::pow(NumberOfStratifications, n);
-
-            // Set the cout to print fixed float point numbers with no digits after the decimal
-            std::cout << std::fixed << std::setprecision(0);
-
-            // Output the templated struct with both NumberOfStratifications and NumberOfHyperCubes
-            std::cout << "template<> struct ValueMapper<" << x << "," << n << "> {\n"
-                      << "    static constexpr int NumberOfStratifications = " << NumberOfStratifications << ";\n"
-                      << "    static constexpr long long NumberOfHyperCubes = " << NumberOfHyperCubes << ";\n"
-                      << "};" << std::endl;
-        }
-    }
+    constexpr int x = 1000; // value to find the root of
+    constexpr int n = 2;       // root degree
+    constexpr auto root = nth_root(x, n);
+    EXPECT_EQ(root, std::floor(std::pow(x, 1.0/n)));
 }
